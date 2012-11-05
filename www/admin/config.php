8b786b19b6ef20ab5ef96b11152610d3b5466eeb
Ivan Aleksanov <zloi.gremlin@gmail.com>
2012-11-05 13:35:46 +0400

    files

---
+++ www/admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://chlounge.ru/admin/');
+define('HTTP_CATALOG', 'http://chlounge.ru/');
+define('HTTP_IMAGE', 'http://chlounge.ru/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://chlounge.ru/admin/');
+define('HTTPS_CATALOG', 'http://chlounge.ru/');
+define('HTTPS_IMAGE', 'http://chlounge.ru/image/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:\home\chlounge.ru\www/admin/');
+define('DIR_SYSTEM', 'Z:\home\chlounge.ru\www/system/');
+define('DIR_DATABASE', 'Z:\home\chlounge.ru\www/system/database/');
+define('DIR_LANGUAGE', 'Z:\home\chlounge.ru\www/admin/language/');
+define('DIR_TEMPLATE', 'Z:\home\chlounge.ru\www/admin/view/template/');
+define('DIR_CONFIG', 'Z:\home\chlounge.ru\www/system/config/');
+define('DIR_IMAGE', 'Z:\home\chlounge.ru\www/image/');
+define('DIR_CACHE', 'Z:\home\chlounge.ru\www/system/cache/');
+define('DIR_DOWNLOAD', 'Z:\home\chlounge.ru\www/download/');
+define('DIR_LOGS', 'Z:\home\chlounge.ru\www/system/logs/');
+define('DIR_CATALOG', 'Z:\home\chlounge.ru\www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'china');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'china');
+define('DB_PREFIX', 'oc');
+?>
---
+++ www/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://chlounge.ru/');
+define('HTTP_IMAGE', 'http://chlounge.ru/image/');
+define('HTTP_ADMIN', 'http://chlounge.ru/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://chlounge.ru/');
+define('HTTPS_IMAGE', 'http://chlounge.ru/image/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:\home\chlounge.ru\www/catalog/');
+define('DIR_SYSTEM', 'Z:\home\chlounge.ru\www/system/');
+define('DIR_DATABASE', 'Z:\home\chlounge.ru\www/system/database/');
+define('DIR_LANGUAGE', 'Z:\home\chlounge.ru\www/catalog/language/');
+define('DIR_TEMPLATE', 'Z:\home\chlounge.ru\www/catalog/view/theme/');
+define('DIR_CONFIG', 'Z:\home\chlounge.ru\www/system/config/');
+define('DIR_IMAGE', 'Z:\home\chlounge.ru\www/image/');
+define('DIR_CACHE', 'Z:\home\chlounge.ru\www/system/cache/');
+define('DIR_DOWNLOAD', 'Z:\home\chlounge.ru\www/download/');
+define('DIR_LOGS', 'Z:\home\chlounge.ru\www/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'china');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'china');
+define('DB_PREFIX', 'oc');
+?>
